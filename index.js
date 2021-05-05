e6e89f2d75bbeca6c87985d80d59e105efd2a550
Razvan <razvanmudura25@gmail.com>
2021-05-05 22:42:15 +0300

    database

--- index.js
+++ index.js
@@ -19,11 +19,55 @@ var brickOffsetTop = 35;
 var brickOffsetLeft = 35;
 var score = 0;
 var lives = 5;
-var level = 4;
+var level = 5;
 var bricks = [];
 var brickColor = "#FA1E46";
 var pause = false;
 var randomN = 1;
+var levelSet = [];
+
+const data = {
+  level1: [
+    [0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0],
+    [0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0],
+    [0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0],
+    [0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0],
+  ],
+  level2: [
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1],
+    [1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1],
+    [1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1],
+    [1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+  ],
+  level3: [
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
+    [0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0],
+    [0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0],
+    [0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+  ],
+  level4: [
+    [0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0],
+    [0, 0, 1, 0, 0, 0, 0, 1, 1, 0, 0],
+    [0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0],
+    [0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0],
+    [0, 0, 1, 0, 0, 0, 0, 1, 1, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0],
+  ],
+  level5: [
+    [0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0],
+    [0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0],
+    [0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0],
+    [0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+  ],
+};
 
 //-DOCUMENT EVENTS-
 document.addEventListener("mousemove", mouseMoveHandler, false);
@@ -77,81 +121,38 @@ function initializeBricks() {
   switch (level) {
     case 1:
       brickColor = "#0DDBD4";
-      for (var c = 0; c < brickColumnCount; c++) {
-        bricks[c] = [];
-        for (var r = 0; r < brickRowCount; r++) {
-          if (c == 2 || r == 2 || r == 8)
-            bricks[c][r] = { x: 0, y: 0, status: 1 };
-          else bricks[c][r] = { x: 0, y: 0, status: 0 };
-        }
-      }
+      levelSet = data.level1;
       break;
+
     case 2:
       brickColor = "#38C20A";
-      for (var c = 0; c < brickColumnCount; c++) {
-        bricks[c] = [];
-        for (var r = 0; r < brickRowCount; r++) {
-          if (c == 0 || c == brickColumnCount - 1 || r % 2 == 0)
-            bricks[c][r] = { x: 0, y: 0, status: 1 };
-          else bricks[c][r] = { x: 0, y: 0, status: 0 };
-        }
-      }
+      levelSet = data.level2;
       break;
+
     case 3:
       brickColor = "#DBD82F";
-      for (var c = 0; c < brickColumnCount; c++) {
-        bricks[c] = [];
-        for (var r = 0; r < brickRowCount; r++) {
-          if (
-            (c > r && c < brickRowCount / 2 && r < brickRowCount / 2) ||
-            c + r >= brickRowCount
-          )
-            bricks[c][r] = { x: 0, y: 0, status: 0 };
-          else bricks[c][r] = { x: 0, y: 0, status: 1 };
-        }
-      }
+      levelSet = data.level3;
       break;
+
     case 4:
       brickColor = "#EB7000";
-      for (var c = 0; c < brickColumnCount; c++) {
-        bricks[c] = [];
-        for (var r = 0; r < brickRowCount; r++) {
-          if (
-            (c >= 1 && c <= 4 && r >= 2 && r <= 9) ||
-            ((c == 0 || c == 5) && r == 7)
-          )
-            bricks[c][r] = { x: 0, y: 0, status: 1 };
-          else bricks[c][r] = { x: 0, y: 0, status: 0 };
-        }
-      }
-      bricks[1][9] = { x: 0, y: 0, status: 0 };
-      bricks[4][9] = { x: 0, y: 0, status: 0 };
-      bricks[1][3] = { x: 0, y: 0, status: 0 };
-      bricks[1][4] = { x: 0, y: 0, status: 0 };
-      bricks[1][5] = { x: 0, y: 0, status: 0 };
-      bricks[1][6] = { x: 0, y: 0, status: 0 };
-      bricks[4][3] = { x: 0, y: 0, status: 0 };
-      bricks[4][4] = { x: 0, y: 0, status: 0 };
-      bricks[4][5] = { x: 0, y: 0, status: 0 };
-      bricks[4][6] = { x: 0, y: 0, status: 0 };
+      levelSet = data.level4;
       break;
+
     case 5:
       brickColor = "#EB1601";
-      for (var c = 0; c < brickColumnCount; c++) {
-        bricks[c] = [];
-        for (var r = 0; r < brickRowCount; r++) {
-          if (
-            ((c == 0 || c == brickColumnCount - 1) && r == 5) ||
-            ((c == 1 || c == brickColumnCount - 2) && r >= 4 && r <= 6) ||
-            ((c == 2 || c == brickColumnCount - 3) && r >= 3 && r <= 7)
-          )
-            bricks[c][r] = { x: 0, y: 0, status: 1 };
-          else bricks[c][r] = { x: 0, y: 0, status: 0 };
-        }
-      }
+      levelSet = data.level5;
       break;
     default:
       break;
+  }
+
+  for (var c = 0; c < brickColumnCount; c++) {
+    bricks[c] = [];
+    for (var r = 0; r < brickRowCount; r++) {
+      if (levelSet[c][r] == 1) bricks[c][r] = { x: 0, y: 0, status: 1 };
+      else bricks[c][r] = { x: 0, y: 0, status: 0 };
+    }
   }
 }
 
@@ -168,6 +169,13 @@ function drawBall() {
 
 //-DESENARE PADELA-
 function drawPaddle() {
+  drawBorder(
+    paddleX,
+    canvas.height - paddleHeight,
+    paddleWidth,
+    paddleHeight,
+    2
+  );
   ctx.beginPath();
   ctx.rect(paddleX, canvas.height - paddleHeight, paddleWidth, paddleHeight);
   ctx.fillStyle = "#11DBF0";
